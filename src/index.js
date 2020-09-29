61679f75563ba90fcbcb542ce697c9985a84b286
Majorovaira <majorovaira8@gmail.com>
2020-09-29 23:22:53 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
+  
+    if(array !== undefined && array.length !== 0) {
+        let result = array[0];
+        for ( let i = 0; i < array.length; i ++) {
+            result = Math.min(result, array[i]);
+        }
+        return result;
+    }
   return 0;
 }
 
 exports.max = function max (array) {
+  
+    if(array !== undefined && array.length !== 0) {
+        let result = array[0];
+        for ( let i = 0; i < array.length; i ++) {
+            result = Math.max(result, array[i]);
+        }
+        return result;
+    }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  
+    if(array !== undefined && array.length !== 0) {
+        let result = 0;
+        for ( let i = 0; i < array.length; i ++) {
+            result += array[i];
+        }
+        return result / array.length;
+    }
   return 0;
 }
